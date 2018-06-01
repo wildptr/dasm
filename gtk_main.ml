7a51a6bfad5718221995613c95f06d83f622654a
wildptr <13061136@buaa.edu.cn>
2018-06-01 08:54:57 +0800

    read_u32 returns int32

--- gtk_main.ml
+++ gtk_main.ml
@@ -159,7 +159,7 @@ let show_ssa view () =
 let goto_function view va =
   let db = view.db in
   let proc =
-    match BatHashtbl.find_option db.Database.proc_table va with
+    match Hashtbl.find_option db.Database.proc_table va with
     | Some proc -> proc
     | None ->
       let cfg = Build_cfg.build_cfg db va Nativeint.(to_int (va-0x400000n)) in
@@ -203,13 +203,13 @@ let read_u16 s offset =
   (int_of_char s.[offset  ]       ) lor
   (int_of_char s.[offset+1] lsl  8)
 
-(* works only on 64-bit platforms *)
 let read_u32 s offset =
+  let module I = Int32 in
   if String.length s < offset+4 then raise Invalid_offset;
-  (                     (s.[offset  ] |> int_of_char |> Nativeint.of_int)   ) |> Nativeint.logor
-  (Nativeint.shift_left (s.[offset+1] |> int_of_char |> Nativeint.of_int)  8) |> Nativeint.logor
-  (Nativeint.shift_left (s.[offset+2] |> int_of_char |> Nativeint.of_int) 16) |> Nativeint.logor
-  (Nativeint.shift_left (s.[offset+3] |> int_of_char |> Nativeint.of_int) 24)
+  (             (s.[offset  ] |> int_of_char |> I.of_int)   ) |> I.logor
+  (I.shift_left (s.[offset+1] |> int_of_char |> I.of_int)  8) |> I.logor
+  (I.shift_left (s.[offset+2] |> int_of_char |> I.of_int) 16) |> I.logor
+  (I.shift_left (s.[offset+3] |> int_of_char |> I.of_int) 24)
 
 let () =
   Printexc.record_backtrace true;
@@ -223,11 +223,11 @@ let () =
 
   let entry_point =
     try
-      if not (BatString.starts_with code "MZ") then begin
+      if not (String.starts_with code "MZ") then begin
         Format.eprintf "invalid DOS executable signature@.";
         exit 1
       end;
-      let e_lfanew = read_u32 code 0x3c |> Nativeint.to_int in
+      let e_lfanew = read_u32 code 0x3c |> Int32.to_int in
       let pe_signature = String.sub code e_lfanew 4 in
       if pe_signature <> "PE\x00\x00" then begin
         Format.eprintf "invalid PE signature@.";
@@ -243,7 +243,7 @@ let () =
         Format.eprintf "not a PE32 file@.";
         exit 1
       end;
-      read_u32 code (opt_header_offset + 16)
+      read_u32 code (opt_header_offset + 16) |> Nativeint.of_int32
     with Invalid_offset ->
       Format.eprintf "invalid PE file@.";
       exit 1
